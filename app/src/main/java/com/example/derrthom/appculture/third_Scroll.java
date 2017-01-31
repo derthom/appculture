4071dfba29c46749d845518c1386e63b233d1ef0
Derrick Thomas <derrick.thomas18@yahoo.in>
2017-01-31 14:53:25 +0530

    V 1.0

--- app/src/main/java/com/example/derrthom/appculture/third_Scroll.java
+++ app/src/main/java/com/example/derrthom/appculture/third_Scroll.java
@@ -1,11 +1,13 @@
 package com.example.derrthom.appculture;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class third_Scroll extends AppCompatActivity {
 
@@ -15,7 +17,15 @@ public class third_Scroll extends AppCompatActivity {
         setContentView(R.layout.activity_third__scroll);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        Button next=(Button)findViewById(R.id.next1);
+        next.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view)
+            {
+                Intent intent=new Intent(third_Scroll.this,tab_four.class);
+                startActivity(intent);
+            }
+        });
 
         }
     }
